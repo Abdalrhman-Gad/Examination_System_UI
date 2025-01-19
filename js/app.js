3d94281377228d0e7f08adbd1181f848ffacec77
Amr434 <amrkasban37@gmail.com>
2025-01-19 19:28:40 +0200

    structure

--- js/app.js
+++
@@ -1,9 +0,0 @@
-import { FirstPageView } from "./view/firstPageView.js";
-
-const routes = {
-  a: FirstPageView.renderFirstPage,
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.appendChild(routes.a());
-});
