6a4e31561ee34cbcd0dcc0db70a5d8d007fa51bf
Abdalrhman Gad <126109248+Abdalrhman-Gad@users.noreply.github.com>
2025-01-19 20:26:39 +0200

    side bar added 1.0

---
+++ assets/scripts/components/sidebar.js
@@ -0,0 +1,14 @@
+export function renderSidebar(mainContent) {
+  mainContent = `
+  <div class="sidebar-container w-25 vh-100">
+        <div class="text-white fw-bold fs-5 pt-5  text-center">Examination System</div>
+        <div class="d-flex sidebar d-flex flex-column justify-content-center 
+                    align-items-center text-white h-75 ">
+            <a href="#" class="text-white fw-bold fs-5 py-3  text-center w-100">Exam</a>
+            <a href="#" class="text-white py-3 fs-5  text-center w-100">Add Exam</a>
+            <a href="#" class="text-white py-3 fs-5  text-center w-100">Settings</a>
+            <a href="#" class="text-white py-3 fs-5  text-center w-100">Logout</a>
+        </div>
+    </div>
+  `;
+}
