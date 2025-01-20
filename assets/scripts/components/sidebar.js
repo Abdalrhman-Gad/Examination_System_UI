39a9d0cc2103a9d3ec180798aaf5614dde377c17
Abdalrhman Gad <126109248+Abdalrhman-Gad@users.noreply.github.com>
2025-01-20 15:16:30 +0200

    index updated by gad

--- assets/scripts/components/sidebar.js
+++ assets/scripts/components/sidebar.js
@@ -4,7 +4,7 @@ export function renderSidebar() {
   loadCSS("./assets/styles/sidebar.css");
   const sidebar = document.getElementById("sidebar");
   sidebar.innerHTML = `
-  <div class="sidebar-container w-25 vh-100">
+  <div class="sidebar-container w-100 vh-100">
         <div class="text-white fw-bold fs-5 pt-5  text-center">Examination System</div>
         <div class="d-flex sidebar d-flex flex-column justify-content-center 
                     align-items-center text-white h-75 ">
