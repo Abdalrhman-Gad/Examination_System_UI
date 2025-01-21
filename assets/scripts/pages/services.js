b1b5ac0607b977b57f755c09f5e2b0c68879a728
Amr434 <amrkasban37@gmail.com>
2025-01-21 21:17:35 +0200

    add home page v1.0

--- assets/scripts/pages/services.js
+++ assets/scripts/pages/services.js
@@ -1,21 +1,27 @@
 import { loadCSS } from "../utils/cssloader.js";
 export function renderServices(mainContent) {
-  loadCSS('./assets/styles/services.css')
+  loadCSS("./assets/styles/services.css");
 
-  mainContent.innerHTML = `<div class="row p-5">
+  mainContent.innerHTML = `<div class="row p-5 h-75">
               <h2 class="header">SERVICES</h2>
               <p class="s-description">
                 This is a Website for Online Examanition that students can
                 attend exams online
               </p>
-              <div class="col-4 sevices">
-                <i class="bi bi-star-fill"></i
-                ><span>Taking exams from students</span>
-                <i class="bi bi-star-fill"></i
-                ><span>making exams for stidents</span>
-                <i class="bi bi-star-fill"></i
-                ><span>students can give exams online</span>
-              </div>
+             
+                <div class="col-4 sevices">
+                  <ul>
+                    <li>
+                      <span>Taking exams from students</span>
+                    </li>
+                    <li>
+                      <span>making exams for stidents</span>
+                    </li>
+                    <li>
+                      <span>students can give exams online</span>
+                    </li>
+                  </ul>
+               </div> 
               <div class="col-6 image">
                 <img src="./assets/images/image2.png" alt="" />
               </div>
